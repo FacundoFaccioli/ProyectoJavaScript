fc9ce38a5a01a8eb355d83b00177a8592a56a2b5
FacundoFaccioli <135562203+FacundoFaccioli@users.noreply.github.com>
2023-09-26 17:37:36 -0300

    Objetos + arrays

--- js/main.js
+++ js/main.js
@@ -1,42 +1,6 @@
-let consumo = parseInt(prompt("¿Cuantos kWh de luz consumiste este mes según la factura?"))
+const idsDeClienteValidos = [123, 456, 789, 6655]
 
-function calcularHasta120(consumo) {
-    const precioPrimeros60Kwh = 	0.08686
-    const precioSiguientes60Kwh = 0.11514
-
-    let precioAPagar
-    if(consumo > 60 && consumo < 120){
-        precioAPagar = (60* precioPrimeros60Kwh) + ((consumo - 60)* precioSiguientes60Kwh)
-    } else {
-        precioAPagar = consumo * precioPrimeros60Kwh
-    }
-    return {
-        precioAPagar,
-        montoExtra: calcularMontoExtraAleatorio()
-    }
-}
-
-function calcularDesde120(consumo) {
-    const precioPrimeros60Kwh = 0.09347
-    const precioSiguientes60Kwh = 0.11932
-    const precioExcedente120Kwh = 0.22030
-
-    let precioAPagar
-    if(consumo > 60 && consumo <120){
-        precioAPagar = (60* precioPrimeros60Kwh) + ((consumo - 60)* precioSiguientes60Kwh)
-    }
-    if(consumo > 120){
-        precioAPagar = (60* precioPrimeros60Kwh) + (60* precioSiguientes60Kwh) + ((consumo - 120)* precioExcedente120Kwh)
-    } else {
-        precioAPagar = consumo * precioPrimeros60Kwh
-    }
-
-    return {
-        precioAPagar,
-        montoExtra: calcularMontoExtraAleatorio()
-    }
-}
-function probabilidadTresPorCiento() {
+function obtenerNumeroAleatorio() {
     const numeroAleatorio = Math.random();
 
     if (numeroAleatorio <= 0.03) {
@@ -50,7 +14,7 @@ function calcularMontoExtraAleatorio() {
     let precioExtra = 0
 
     for (let i = 0; i < 10; i++) {
-        if (probabilidadTresPorCiento()) {
+        if (obtenerNumeroAleatorio()) {
             precioExtra += 37
         }
     }
@@ -58,20 +22,69 @@ function calcularMontoExtraAleatorio() {
     return precioExtra
 }
 
-function dameElPrecioEnBaseAlConsumo(consumo) {
-    if (consumo > 120) {
-        return calcularDesde120(consumo)
-    } else {
-        return calcularHasta120(consumo)
+function FacturaHastaA120Kwh() {
+    this.precioPrimeros60Kwh = 	0.08686
+    this.precioSiguientes60Kwh = 0.11514
+
+    this.calcularPrecioAPagar = function(consumo){
+        let precioAPagar
+        if(consumo>60 && consumo <120){
+            precioAPagar = (60*this.precioPrimeros60Kwh) + ((consumo - 60)*this.precioSiguientes60Kwh)
+        } else {
+            precioAPagar = consumo * this.precioPrimeros60Kwh
+        }
+        return {
+            precioAPagar,
+            montoExtra: calcularMontoExtraAleatorio()
+        }
     }
 }
 
-const resultado = dameElPrecioEnBaseAlConsumo(consumo)
+function FacturaMayorA120Kwh() {
+    this.precioPrimeros60Kwh = 0.09347
+    this.precioSiguientes60Kwh = 0.11932
+    this.precioExcedente120Kwh = 0.22030
+
+    this.calcularPrecioAPagar = function (consumo) {
+        let precioAPagar
+        if(consumo>60 && consumo <120){
+            precioAPagar = (60*this.precioPrimeros60Kwh) + ((consumo - 60)*this.precioSiguientes60Kwh)
+        }
+        if(consumo>120){
+            precioAPagar = (60*this.precioPrimeros60Kwh) + (60*this.precioSiguientes60Kwh) + ((consumo - 120)*this.precioExcedente120Kwh)
+        } else {
+            precioAPagar = consumo * this.precioPrimeros60Kwh
+        }
+
+        return {
+            precioAPagar,
+            montoExtra: calcularMontoExtraAleatorio()
+        }
+    }
+}
+
+function obtenerFactura(consumo) {
+    if (consumo > 120) {
+        return new FacturaMayorA120Kwh()
+    } else {
+        return new FacturaHastaA120Kwh()
+    }
+}
+
+let clienteId = parseInt(prompt("Por favor ingrese su ID de cliente"))
+
+if (idsDeClienteValidos.includes(clienteId)) {
+    const consumo = parseInt(prompt("¿Cuantos kWh de luz consumiste este mes según la factura?"))
+    const factura = obtenerFactura(consumo)
+    const resultado = factura.calcularPrecioAPagar(consumo)
 
 
-console.log("El precio a pagar de luz este mes es de $ " + resultado.precioAPagar)
-if (resultado.montoExtra == 0) {
-    console.log("No corresponde monto extra este mes :)")
+    console.log(`El precio a pagar de luz este mes es de ${resultado.precioAPagar}`)
+    if (resultado.montoExtra === 0) {
+        console.log("No corresponde monto extra este mes :)")
+    } else {
+        console.log("El monto extra por subidas de tensión es de: " + resultado.montoExtra + " pesos")
+    }
 } else {
-    console.log("El monto extra por subidas de tensión es de: $ " + resultado.montoExtra)
+    console.log("El cliente no pertenece a la compañía")
 }

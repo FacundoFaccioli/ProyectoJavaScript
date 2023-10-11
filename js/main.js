a958623c773ee203065526662aaaa3ab706e7047
FacundoFaccioli <135562203+FacundoFaccioli@users.noreply.github.com>
2023-10-11 17:40:58 -0300

    DOM + JSON

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-const idsDeClienteValidos = [123, 456, 789, 6655]
+const idsDeClienteValidos = [
+    {
+        id: "123",
+        descuento: 0.1
+    },
+    {
+        id: "AA456",
+        descuento: 0.2
+    },
+    {
+        id: "789",
+        descuento: 0.05
+    },
+    {
+        id: "6655",
+        descuento: 0.15
+    }
+]
+
+const ARSFormateador = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: 'ARS',
+});
 
 function obtenerNumeroAleatorio() {
     const numeroAleatorio = Math.random();
@@ -71,20 +93,55 @@ function obtenerFactura(consumo) {
     }
 }
 
-let clienteId = parseInt(prompt("Por favor ingrese su ID de cliente"))
+function actualizarIdDeUsuario() {
+    if (localStorage.getItem("clienteId") != null) {
+        const usuario = JSON.parse(localStorage.getItem("clienteId"))
+        document.getElementById("idDeUsuario").innerHTML = "Tu id de usuario es: " + usuario.id
+        document.getElementById("erroresUsuario").innerHTML = ""
+    } else {
+        document.getElementById("idDeUsuario").innerHTML = "No hay ID de cliente definido"
+    }
+}
 
-if (idsDeClienteValidos.includes(clienteId)) {
-    const consumo = parseInt(prompt("¿Cuantos kWh de luz consumiste este mes según la factura?"))
+document.getElementById("definirId").addEventListener("click", function () {
+    const clienteId = document.getElementById("clienteId").value
+    if (clienteId === "") {
+        document.getElementById("erroresUsuario").innerHTML = "Por favor ingrese un ID de cliente"
+    } else {
+        const cliente = idsDeClienteValidos.find((id) => id.id === clienteId)
+        if (cliente) {
+            localStorage.setItem("clienteId", JSON.stringify(cliente));
+            actualizarIdDeUsuario()
+        } else {
+            document.getElementById("erroresUsuario").innerHTML = "El id no pertenece a la compañía"
+        }
+    }
+})
+
+document.getElementById("botonCalcular").addEventListener("click", function () {
+    const clienteString = localStorage.getItem("clienteId")
+    if (clienteString  == null) {
+        document.getElementById("erroresDeCalculo").innerHTML = "Por favor defina su ID de cliente"
+        return
+    } else {
+        document.getElementById("erroresDeCalculo").innerHTML = ""
+    }
+
+    const cliente = JSON.parse(clienteString)
+
+    const consumo = parseInt(document.getElementById("consumoCliente").value)
     const factura = obtenerFactura(consumo)
     const resultado = factura.calcularPrecioAPagar(consumo)
 
+    document.getElementById("totalAPagar").innerHTML = "El precio a pagar de luz este mes es de " + ARSFormateador.format(resultado.precioAPagar - (resultado.precioAPagar * cliente.descuento)) +
+        ".</br> Se te aplico un descuento del " + (cliente.descuento * 100) + "% (" + ARSFormateador.format(resultado.precioAPagar * cliente.descuento) + ")"
 
-    console.log(`El precio a pagar de luz este mes es de ${resultado.precioAPagar}`)
     if (resultado.montoExtra === 0) {
-        console.log("No corresponde monto extra este mes :)")
+        document.getElementById("totalAPagar").innerHTML += ". <br/>No corresponde monto extra este mes :)" + "."
     } else {
-        console.log("El monto extra por subidas de tensión es de: " + resultado.montoExtra + " pesos")
+        document.getElementById("totalAPagar").innerHTML += ". <br/>El monto extra por subidas de tensión es de: " + ARSFormateador.format(resultado.montoExtra) + "."
     }
-} else {
-    console.log("El cliente no pertenece a la compañía")
-}
+})
+
+// let clienteId = parseInt(prompt("Por favor ingrese su ID de cliente"))
+actualizarIdDeUsuario()
